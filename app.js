6d729fd5de03cbd85a0a34272066f963d9f65a00
Panupong Surarat (PS Cloud) - Synnex Thailand <panupong_su@Synnex.co.th>
2025-03-06 17:15:00 +0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Until then'));
+app.get('/', (req, res) => res.send('Missing'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
